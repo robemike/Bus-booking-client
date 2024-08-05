912eb3c53a23a21e6052419f45e8663ac0e84efe
Ian Kamau <iankamau112@gmail.com>
2024-08-05 11:09:27 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import FindBus1 from './Components/Findbus';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <FindBus1 />
     </div>
   );
