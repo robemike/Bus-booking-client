e2a656b2b0f0a1ce86a894847f8449127c32c5a3
Laurine Maina <127749979+laureenmaina@users.noreply.github.com>
2024-08-08 15:30:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Seats from './components/customer/seats';
 import HomePage from "./components/customer/HomePage";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Buses from "./components/Drivers/buses";
+import DriverBuses from "./components/Drivers/buses";
 import Landing from "./components/Drivers/landing";
 import CustomerList from "./components/Drivers/customerlist";
 import Navbar from "./components/Drivers/Navbar";
@@ -38,7 +38,7 @@ function App() {
         <Route path="/customer" element={<Customer />} />
         <Route path="/drivers/navbar" element={<Navbar />} />
         <Route path="/drivers/landing" element={<Landing />} />
-        <Route path="/drivers/buses" element={<Buses />} />
+        <Route path="/drivers/buses" element={<DriverBuses />} />
         <Route path="/drivers/customer-list" element={<CustomerList/>} />
         <Route path="/drivers/trips" element={<Trips />} />
           
