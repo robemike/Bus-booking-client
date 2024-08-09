35697e8fefd9c31bbfebdd79ebbe36f76983f9cb
Laurine Maina <mainalaureen58@gmail.com>
2024-08-09 09:36:00 +0300

    Added more stylings

--- src/components/customer/HomePage.js
+++ src/components/customer/HomePage.js
@@ -3,13 +3,14 @@ import Navbar from "./Navbar";
 import Destinations from "./Destinations";
 import buses from "../../assets/buses.jpg";
 import BookingForm from "./BookingForm";
+import './Homepage.css'
 
 const HomePage = () => {
   return (
     <div>
       {/* <Navbar /> */}
       <div className="pt-16">
-        <img
+        <img id="homeimage"
           src={buses}
           alt="buses"
           className="w-full h-[600px] object-cover"
